b03b200b69288af0f6fde8228fe8434a5dcedb6a
Chris Allen <cma@bitemyapp.com>
2025-09-26 17:00:55 -0500

    Ditch the sleep in the dyn lib call

--- crates/inter_two_crates/writerlib/src/lib.rs
+++ crates/inter_two_crates/writerlib/src/lib.rs
@@ -3,8 +3,6 @@ use std::io::Write;
 /// Exported from a separate crate to test cross-crate optimizations.
 #[inline(never)]
 pub fn write_bytes_dyn_lib(w: &mut dyn Write, buf: &[u8]) {
-    std::thread::sleep(std::time::Duration::from_millis(1_000));
-
     for &b in buf {
         let _ = w.write_all(&[b]);
     }
